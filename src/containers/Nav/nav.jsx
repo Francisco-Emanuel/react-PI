fb4f15fe79c2cc7db8769d5008cf1642466d5d9a
Francisco <112523765+Francisco-Emanuel@users.noreply.github.com>
2026-02-25 01:04:49 +0000

    caos

---
+++ src/containers/Nav/nav.jsx
@@ -0,0 +1,13 @@
+import './nav.css';
+
+export const Nav = () => {
+    return (
+            <nav>
+                <ul>
+                    <li><a href="#">item</a></li>
+                    <li><a href="#">item2</a></li>
+                    <li><a href="#">item3</a></li>
+                </ul>
+            </nav>
+    )
+}
